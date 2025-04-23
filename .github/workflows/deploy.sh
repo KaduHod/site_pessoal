46773aab54242e0538406648ae6287e43fbd2c8f
kaduhod <carlosjr.ribas@gmail.com.br>
2025-04-23 19:18:31 -0300

    :bug:

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-ssh deployer@172.17.0.2 <<EOF
+ssh deployer@172.17.0.2 <<'EOF'
+    set -e  # Faz o script falhar se algum comando falhar
+
+    sudo apt-get update
+    sudo apt-get install -y unzip
+
     rm -rf meu_site_deploy
-    mkir -p meu_site_deploy
-    ls -lah | grep meu_site
+    mkdir -p meu_site_deploy
+
     unzip app.zip -d meu_site_deploy
     cp -r meu_site_deploy/* /var/www/html/meu-site/
 EOF
