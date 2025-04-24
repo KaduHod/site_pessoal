28f3b50369267ec86f856121af9f3d38c25b62c7
kaduhod <carlosjr.ribas@gmail.com.br>
2025-04-23 23:34:34 -0300

    :bug: [fix] comando unzip

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 ssh deployer@172.17.0.2 <<EOF
     set -e # Faz o script falhar se algum comando falhar
-    echo "O q ta aconteceno"
-    sudo apt-get update
-    sudo apt-get install -y unzip
-
     rm -rf meu_site_deploy
     mkdir -p meu_site_deploy
 
     unzip -o app.zip -d meu_site_deploy
+    rm -rf /var/www/html/meu-site/*
+    mv /var/www/html/meu-site/* /var/www/html/meu-site-backup/
     cp -r meu_site_deploy/* /var/www/html/meu-site/
 EOF
