eafc6f0c0620883779f652ed56daf3d431664131
kaduhod <carlosjr.ribas@gmail.com.br>
2025-04-25 15:57:48 -0300

    corrigido cp em dir incorreto

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ ssh -t deployer@172.17.0.2 <<EOF
     unzip -o app.zip -d meu_site_deploy
     rm -rf /var/www/html/meu-site/*
     echo "Movendo arquivos para o diretorio alvo do nginx"
-    cp -r meu_site_deploy/dist/* /var/www/html/meu-site/
+    cp -r meu_site_deploy/* /var/www/html/meu-site/
 EOF
