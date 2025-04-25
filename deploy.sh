131d98a761b902227b93863e3bbe3c5fc8c3bd94
kaduhod <carlosjr.ribas@gmail.com.br>
2025-04-25 16:11:44 -0300

    :bug: [fix]

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh -t deployer@172.17.0.2 <<EOF
     mkdir -p meu_site_deploy
     mkdir -p /var/www/html/meu-site
     unzip -o app.zip -d meu_site_deploy
-    ls -lah -tr mwu_site_deploy
+    ls -lah -tr meu_site_deploy
     rm -rf /var/www/html/meu-site/*
     echo "Movendo arquivos para o diretorio alvo do nginx"
     cp -r meu_site_deploy/dist/* /var/www/html/meu-site/
