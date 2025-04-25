486de94fca6c5b8620992c41b12d929065d9fdd3
kaduhod <carlosjr.ribas@gmail.com.br>
2025-04-25 16:42:49 -0300

    :bug:

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ ssh -t deployer@172.17.0.2 <<EOF
     ls -lah -tr meu_site_deploy
     rm -rf /var/www/html/meu-site/*
     echo "Movendo arquivos para o diretorio alvo do nginx"
-    cp -r meu_site_deploy/dist/* /var/www/html/meu-site/
+    cp -r meu_site_deploy/* /var/www/html/meu-site/
 EOF
